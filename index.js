88cb5dcfffde29752b33a09375d4999222da19e2
n0social <38169198+n0social@users.noreply.github.com>
2024-03-13 06:18:37 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Made it.");
+    res.send("<h1>Hello</h1> </br> <h2>Bon Jour</h2>"); //Send response
+    console.log(req, rawHeaders); //Listen for computer access to the server
+});
+
+app.get("/about", (req, res) => { //localhost:3000/about
+    res.send("<h1>About me</h1>"); //Send response
+});
+
+app.get("/contact", (req, res) => { //localhost:3000/contact
+    res.send("<h1>email@gmail.com</h1>"); //Send response
 });
 
 app.listen(3000, () => {
